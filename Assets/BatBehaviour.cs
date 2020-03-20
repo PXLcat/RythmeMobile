c20a2a4c057daf67537323f18f682cfda64c447d
PXLcat <pixellecat@gmail.com>
2020-03-20 12:22:25 +0100

    partage

---
+++ Assets/BatBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatBehaviour : MonoBehaviour
+{
+    private Transform _transform;
+
+    private void Awake()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        transform.position += new Vector3(-0.1f, 0, 0);
+    }
+
+    private void GetHit()
+    {
+        //trigger animation hurt
+        //
+    }
+}
---
+++ Assets/SpawnEnemyWithPool.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemyWithPool : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> _enemies;
+    public int m_maxEnemies = 10;
+    public GameObject m_enemyPrefab;
+
+    private void Awake()
+    {
+        for (int i = 0; i < m_maxEnemies; i++)
+        {
+            _enemies.Add(Instantiate(m_enemyPrefab, transform));
+        }
+    }
+
+}
